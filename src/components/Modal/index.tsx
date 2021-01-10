800196ed98244f55ac38b0e2732822e33376f0f3
Alexander <alexbidenko1998@gmail.com>
2021-01-10 22:23:48 +0300

    Tutorial 14

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,33 @@
 import {CSSTransition} from "react-transition-group";
 import React, {useMemo, Suspense, lazy} from "react";
 import {createPortal} from "react-dom";
-import style from './Modal.module.css';
 import './ModalTransition.css';
 import {RedactCardProps} from "../RedactCard";
+import styled from 'styled-components';
 
 const RedactCard = lazy(() => import("../RedactCard"));
+
+const ModalOverlay = styled.div`
+    background-color: rgba(0,0,0,.64);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`;
+
+const ModalCard = styled.div`
+    background-color: #f4f5f7;
+    border-radius: 2px;
+    overflow: hidden;
+    width: 768px;
+    z-index: 25;
+    height: 400px;
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 48px;
+`;
 
 type ModalProps = {
     isOpen: boolean;
@@ -19,13 +41,13 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, ...props }) => {
 
     return createPortal(
         <CSSTransition in={isOpen} timeout={300} classNames="modalTransition" unmountOnExit>
-            <div className={style.modal}>
-                <div className={style.modal__overlay} onClick={onClose} />
-                <div className={style.modal__card}>
+            <div>
+                <ModalOverlay onClick={onClose} />
+                <ModalCard>
                     <Suspense fallback="Загрузка...">
                         <RedactCard {...props} />
                     </Suspense>
-                </div>
+                </ModalCard>
             </div>
         </CSSTransition>,
         modals,
